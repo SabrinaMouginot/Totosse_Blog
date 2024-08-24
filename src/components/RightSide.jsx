426ffcd8e77ce0d6d3fd59a8ddd22a56217180ad
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-08-24 23:24:26 +0200

    creation de la barre droite : jsx et css

---
+++ src/components/RightSide.jsx
@@ -0,0 +1,11 @@
+import '../sass/RightSide.scss'
+
+function RightSide() {
+    return (
+        <div className="right-side">
+            <p>X</p>
+        </div >
+    )
+}
+
+export default RightSide
