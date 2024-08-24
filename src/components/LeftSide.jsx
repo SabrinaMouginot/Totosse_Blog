a2d7aa21c968ce8061dcac1bf2d54f115909a0fa
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-08-24 23:23:57 +0200

    creation de la barre gauche avec la photo de profil : jsx et css

---
+++ src/components/LeftSide.jsx
@@ -0,0 +1,15 @@
+import '../sass/Leftside.scss'
+
+function LeftSide() {
+    return (
+        <div className="left-side">
+            <div className="profile-box">
+                <img src="..//Totosse_profile.png" alt="Profile" className="profile-box" />
+                <h2>Pseudo : <span className='baby'>BABY-TIXXX</span></h2>
+                <p>Age : <span className="baby">8 mois</span></p>
+            </div>
+        </div >
+    )
+}
+
+export default LeftSide
