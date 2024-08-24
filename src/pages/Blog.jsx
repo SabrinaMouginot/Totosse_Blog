7f95d16bba19ad64ead32f6900210e30feea9cd5
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-08-24 23:16:39 +0200

    creation de la page blog

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,8 +1,15 @@
+import LeftSide from "../components/LeftSide"
+import RightSide from "../components/RightSide"
+import '../css/Blog.css'
+
 function Blog() {
     return (
-        <div>
-
-        </div>
+        <div className="blog">
+            <div className="sides">
+                <LeftSide />
+                <RightSide />
+            </div>
+        </div >
     )
 }
 
